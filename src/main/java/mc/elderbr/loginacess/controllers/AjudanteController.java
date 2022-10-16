a7e49329bc2f08c9512b29f0b86b94ef396c4fdf
ElderBR <elderbarbosaromao@gmail.com>
2022-10-16 19:38:18 -0300

    Evento quando o jogador sair do jogo verifica se ele é do tipo Ajudante|Seguidor se for limpa o inventário jogando os itens para o jogador responsável por mesmo

--- src/main/java/mc/elderbr/loginacess/controllers/AjudanteController.java
+++ src/main/java/mc/elderbr/loginacess/controllers/AjudanteController.java
@@ -6,8 +6,17 @@ import mc.elderbr.loginacess.dao.JogadorDao;
 import mc.elderbr.loginacess.exceptions.JogadorException;
 import mc.elderbr.loginacess.model.Ajudante;
 import mc.elderbr.loginacess.model.Amigo;
+import mc.elderbr.loginacess.model.Bau;
+import net.md_5.bungee.api.chat.hover.content.Item;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static mc.elderbr.loginacess.interfaces.JogadorInterface.JOGADOR_MAP;
 import static mc.elderbr.loginacess.interfaces.JogadorInterface.LISTA_AJUDANTE;
@@ -22,6 +31,31 @@ public class AjudanteController {
             if (ajudante.getAmigo() == null) {
                 throw new JogadorException("Ops, tem algo errado, cadê o seu amigo");
             }
+        }
+    }
+
+    public void quit(Player player){
+        if(JOGADOR_MAP.get(player.getName()) instanceof Ajudante ajudante){
+            Player responsavel = Bukkit.getPlayer(ajudante.getAmigo().getUuid());
+            List<ItemStack> listItemStack = new ArrayList<>();
+            for(ItemStack itemStack : player.getInventory()){
+                listItemStack.add(itemStack);
+            }
+            for(ItemStack itemStack : player.getEnderChest()){
+                listItemStack.add(itemStack);
+            }
+            if(responsavel != null){
+                for(ItemStack itemStack : listItemStack) {
+                    Bukkit.getWorld(responsavel.getWorld().getName()).dropItem(responsavel.getLocation(), itemStack);
+                }
+            }else{
+
+                Bau bau = new Bau(player).createBau();
+                bau.addItem();
+
+            }
+            player.getInventory().clear();
+            player.getEnderChest().clear();
         }
     }
 
--- src/main/java/mc/elderbr/loginacess/events/QuitEvent.java
+++ src/main/java/mc/elderbr/loginacess/events/QuitEvent.java
@@ -1,2 +1,15 @@
-package mc.elderbr.loginacess.events;public class QuitEvent {
+package mc.elderbr.loginacess.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class QuitEvent implements Listener {
+
+    private Player player;
+
+    public void quitPlayer(PlayerQuitEvent event){
+        player = event.getPlayer();
+    }
+
 }
