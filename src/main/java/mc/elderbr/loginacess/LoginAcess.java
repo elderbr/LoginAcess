fbda0d863a872eb7efb1a876d9f31b17cf3d5ba3
ElderBR <elderbarbosaromao@gmail.com>
2022-10-16 21:04:53 -0300

    Evento ao jogador sair do jogo, se for seguidor|ajudante os itens vão para o jogador responsavel

--- src/main/java/mc/elderbr/loginacess/LoginAcess.java
+++ src/main/java/mc/elderbr/loginacess/LoginAcess.java
@@ -6,10 +6,7 @@ import mc.elderbr.loginacess.controllers.AmigoController;
 import mc.elderbr.loginacess.controllers.EsperaController;
 import mc.elderbr.loginacess.dao.ConfigDao;
 import mc.elderbr.loginacess.dao.ItemDao;
-import mc.elderbr.loginacess.events.BreakBlockEvent;
-import mc.elderbr.loginacess.events.InventorioClickEvent;
-import mc.elderbr.loginacess.events.JoinEvent;
-import mc.elderbr.loginacess.events.PlayerMove;
+import mc.elderbr.loginacess.events.*;
 import mc.elderbr.loginacess.exceptions.JogadorException;
 import mc.elderbr.loginacess.interfaces.ItemInterface;
 import mc.elderbr.loginacess.model.Ajudante;
@@ -68,6 +65,7 @@ public final class LoginAcess extends JavaPlugin implements Listener, ItemInterf
         getServer().getPluginManager().registerEvents(new PlayerMove(), this);
         getServer().getPluginManager().registerEvents(new BreakBlockEvent(), this);
         getServer().getPluginManager().registerEvents(new InventorioClickEvent(), this);
+        getServer().getPluginManager().registerEvents(new QuitEvent(), this);
     }
 
 
--- src/main/java/mc/elderbr/loginacess/events/QuitEvent.java
+++ src/main/java/mc/elderbr/loginacess/events/QuitEvent.java
@@ -1,15 +1,21 @@
 package mc.elderbr.loginacess.events;
 
+import mc.elderbr.loginacess.controllers.AjudanteController;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class QuitEvent implements Listener {
 
     private Player player;
+    private AjudanteController ajudanteController;
 
+    @EventHandler
     public void quitPlayer(PlayerQuitEvent event){
         player = event.getPlayer();
+        ajudanteController = new AjudanteController();
+        ajudanteController.quit(player);
     }
 
 }
